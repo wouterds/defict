8dec67126a05c9a7ba1c7913d74b826a7d63f8ff
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-22 11:02:10 +0200

    :sparkles: Ethereum.walletBalance()

--- src/services/ethereum.ts
+++ src/services/ethereum.ts
@@ -4,6 +4,16 @@ class EthereumService {
   private web3 = new Web3(
     `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
   );
+
+  public walletBalance = async (address: string): Promise<string | null> => {
+    const balance = await this.web3.eth.getBalance(address);
+
+    if (balance) {
+      return this.web3.utils.fromWei(balance, 'ether');
+    }
+
+    return null;
+  };
 }
 
 export default new EthereumService();
