b98b8b99f76a7721c37f2583c3e6fdaeff5112fe
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-22 11:19:37 +0200

    :sparkles: Fetch & display Ethereum wallet balance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Ethereum from 'services/ethereum';
 
 const Index = () => {
+  const [address, setAddress] = useState('');
+  const [balance, setBalance] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (Ethereum.isValidAddress(address)) {
+      Ethereum.walletBalance(address).then(setBalance);
+    }
+  }, [address]);
+
   return (
     <div>
-      <h1>Hello world!</h1>
+      <h1>Ethereum balance{balance && `: ${balance} ETH`}</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Ethereum address"
+          onChange={(e: any) => setAddress(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
