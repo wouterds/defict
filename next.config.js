b78a342f1e9cab1f026c9a2ed830d22232fb8a64
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-22 10:19:12 +0200

    :wrench: .env support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
+require('dotenv').config();
+const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const typescript = require('@zeit/next-typescript');
+const dotenv = require('dotenv-webpack');
 
 const config = {
   distDir: '../dist',
@@ -8,4 +11,19 @@ const config = {
 
 module.exports = withPlugins([
   [typescript],
-], config);
+], {
+  webpack: (config) => {
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+      new dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ];
+
+    return config;
+  },
+  ...config,
+});
